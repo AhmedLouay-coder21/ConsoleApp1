a8d7a1168483791af1c6ac871f05c873c5e05c4f
AhmedLouay-coder21 <140275085+AhmedLouay-coder21@users.noreply.github.com>
2026-02-06 09:13:02 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float x = 3.4f;
+            float y = 4.9f;
+            float sum = x + y;
+            // This will print sum, which should print 8.3
+            Console.WriteLine(sum);
+        }
+    }
+}
